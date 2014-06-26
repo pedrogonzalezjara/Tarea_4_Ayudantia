0f917727a699d3845c15eb6aa53584323f410e2a
pedrogonzalezjara <pdro.gonzalez@gmail.com>
2014-06-26 18:55:18 -0400

    Agregado script SQL

---
+++ tarea_4.sql
@@ -0,0 +1,19 @@
+﻿
+CREATE TABLE bodega (
+    id serial NOT NULL,
+    nombre_bodega varchar(255) NOT NULL,
+    codigo_bodega varchar(255) NOT NULL,
+    UNIQUE (codigo_bodega),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE productos (
+    id serial NOT NULL,
+    fk_bodega_id serial NOT NULL REFERENCES bodega(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    nombre_producto varchar(255) NOT NULL,
+    codigo_producto varchar(255) NOT NULL,
+    precio_neto varchar(255) NOT NULL,
+    UNIQUE(codigo_producto),
+    PRIMARY KEY (id)
+);
+
